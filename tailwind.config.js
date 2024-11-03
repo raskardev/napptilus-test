c2779ebf900c2bd242558c2d32bfabcbef2b6864
raskardev <alexcdev95@gmail.com>
2024-11-03 13:35:01 +0100

    chore: initial config and assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        header: "var(--header-color)",
+        gender: "var(--gender-color)",
+        profession: "var(--profession-color)",
+      },
+      gridTemplateRows: {
+        "employee-details": "repeat(2, 50%)",
+      },
+    },
   },
   plugins: [],
 };
